fb35599a1a8585b7443e43fe51ed0ce86eb255f1
LiZi-77 <95942789+LiZi-77@users.noreply.github.com>
2022-01-24 00:11:50 +0100

    Update 283.py

--- 数组和矩阵/283.py
+++ 数组和矩阵/283.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def swap(i1, i2, l:List[int]) -> List[int]:
+            a = l[i1]
+            l[i1] = l[i2]
+            l[i2] = a
+            return l            
+        
+        first_index = -1
+        
+        for curr_index in range(len(nums)-1):
+            if nums[curr_index] is 0:
+                if first_index < 0:
+                    first_index = curr_index
+                if nums[curr_index+1] is not 0:
+                    nums = swap(first_index, curr_index+1,nums)
+                    first_index += 1
+                 
